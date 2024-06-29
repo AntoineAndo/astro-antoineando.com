416aca1bcc75e1ffd0e47d9839970b15dfb3616d
Antoine Ando <antoine.ando@gmail.com>
2024-06-29 04:03:38 +0200

    Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   prefetch: true,
+  output: "static",
+  adapter: vercelStatic(),
 });
