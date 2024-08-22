8a701b5458c451e2b0bc24119c45beab2f1a905b
Antoine Ando <antoine.ando@gmail.com>
2024-08-22 15:23:28 +0200

    Socials

--- src/components/icons/Github.tsx
+++ src/components/icons/Github.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export const Github = (props: any) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 98 96"
+    viewBox="0 0 100 100"
     className={props.class}
   >
     <path
