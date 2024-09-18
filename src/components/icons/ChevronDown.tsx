c84c772732c8f72c5aa616ce0aa44421cd76b72d
Antoine Ando <antoine.ando@gmail.com>
2024-09-18 07:01:14 +0200

    Warning fix

--- src/components/icons/ChevronDown.tsx
+++ src/components/icons/ChevronDown.tsx
@@ -10,9 +10,9 @@ export const ChevronDown = (props: any) => (
     <path
       d="M6 9L12 15L18 9"
       stroke="inherit"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
